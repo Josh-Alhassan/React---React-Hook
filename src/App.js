f9c764232691a38ff4c5abe35fc6c9204a197bff
Saint-Josh <saintjosh26@gmail.com>
2022-08-16 20:35:30 +0100

    created card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Card from './Card'
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
